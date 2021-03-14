d936f3c71981b169fcc9d00773808f7da928860c
Alex Ionescu <alxi.2001@gmail.com>
2021-03-14 20:28:15 +0200

    Refactor main()

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,12 @@ fn main() {
     .expect("Please specify the maze file as a command-line argument.");
 
   let (width, height, mut pixels) = load_luma8_parts(&img_path);
-  println!("Img loaded.");
 
   let maze = parse(width, height, &pixels);
-  let end = maze.end;
-  println!("Parsed.");
-
   let prev = solve(&maze);
-  println!("Solved.");
 
   let (path, path_length) = make_path(&maze, width, &prev);
-  println!("Path computed.");
-
-  draw_path(&mut pixels, &path, path_length, end);
-  println!("Path drawn.");
+  draw_path(&mut pixels, &path, path_length, maze.end);
 
   save_solved(width, height, pixels, &img_path)
 }
