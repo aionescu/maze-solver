67ca86bbf14d3eb03c80cf3c878f059f01794aa0
Alex Ionescu <alxi.2001@gmail.com>
2021-03-20 04:38:22 +0200

    Update run-examples script

--- run-examples.sh
+++ run-examples.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in `find Examples -type f | egrep -v 'Solved'`; do
+for file in `find Examples -type f -not -name '*-Solved.*'`; do
   echo "Solving $file"
   cargo run --quiet --release -- "$file" > /dev/null
 done
