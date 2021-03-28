edf3744886af643b6ebf3468e239dfadb6003f6f
Alex Ionescu <alxi.2001@gmail.com>
2021-03-28 17:34:54 +0300

    Improve filepath handling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ mod parser;
 mod solver;
 
 use std::env::args;
-use std::path::Path;
+use std::ffi::OsStr;
+use std::path::{Path, PathBuf};
 
 use image::{open, GrayImage};
 
@@ -11,28 +12,27 @@ use crate::parser::parse;
 use crate::solver::{solve, make_path, draw_path};
 
 #[timed("Loading")]
-fn load_luma8_parts(path: &str) -> (u32, u32, Vec<u8>) {
+fn load_luma8_parts<P: AsRef<Path>>(path: P) -> (u32, u32, Vec<u8>) {
   let img = open(path).unwrap().into_luma8();
   (img.width(), img.height(), img.into_raw())
 }
 
 #[timed("Saving")]
-fn save_luma8_parts(width: u32, height: u32, pixels: Vec<u8>, path: &str) {
+fn save_luma8_parts<P: AsRef<Path>>(width: u32, height: u32, pixels: Vec<u8>, path: P) {
   let img = GrayImage::from_raw(width, height, pixels).unwrap();
   img.save(&path).unwrap()
 }
 
-fn append_solved(path: &str) -> String {
+fn append_solved(path: &str) -> PathBuf {
   let path = Path::new(&path);
-  let parent = path.parent().unwrap().to_str().unwrap();
-  let stem = path.file_stem().unwrap().to_str().unwrap();
-  let ext = path.extension().unwrap().to_str().unwrap();
 
-  if !parent.is_empty() {
-    format!("{}/{}-Solved.{}", parent, stem, ext)
-  } else {
-    format!("{}-Solved.{}", stem, ext)
-  }
+  let stem = path.file_stem().and_then(OsStr::to_str).unwrap_or("");
+  let ext = path.extension().and_then(OsStr::to_str).unwrap_or("");
+
+  path
+    .with_file_name(format!("{}-Solved", stem))
+    .with_extension(ext)
+    .to_owned()
 }
 
 #[timed("Total")]
@@ -49,5 +49,5 @@ fn main() {
   draw_path(&mut pixels, &path, path_length, end);
 
   save_luma8_parts(width, height, pixels, &solution_path);
-  println!("Solution saved to {}.", solution_path)
+  println!("Solution saved to {}.", solution_path.display())
 }
